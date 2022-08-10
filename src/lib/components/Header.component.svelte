646f734cd4036081c06983704fbde3ebb72d6355
Tommy Wong <tommywong.dev@gmail.com>
2022-08-10 23:18:20 +0800

    add header

---
+++ src/lib/components/Header.component.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	const s = {
+		name: 'Tommy Wong',
+		description: 'develops web and mobile apps.'
+	};
+</script>
+
+<header>
+	<section>
+		<h1>{s.name}</h1>
+		<h2>{s.description}</h2>
+	</section>
+</header>
+
+<style>
+	header {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		min-height: 100vh;
+		position: relative;
+	}
+
+	section {
+		padding: 3rem;
+	}
+
+	h1 {
+		color: var(--light-color);
+		font-size: 8rem;
+		margin: 1rem 0;
+	}
+
+	h2 {
+		background: linear-gradient(20deg, var(--primary-color) 35%, var(--secondary-color) 65%);
+		-webkit-text-fill-color: transparent;
+		-webkit-background-clip: text;
+		font-size: 5rem;
+		margin: 0;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Header from '$lib/components/Header.component.svelte';
+</script>
+
+<main>
+	<Header />
+</main>
