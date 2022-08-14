892052f633fb73b980e0bf0787d59f2c6139d94a
Tommy Wong <tommywong.dev@gmail.com>
2022-08-14 21:36:09 +0800

    add tech component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,16 +3,22 @@
 	import HeaderComponent from '$lib/components/Header.component.svelte';
 	import TechsComponent from '$lib/components/Techs.component.svelte';
 	import graphQLClient from '$lib/graphql/graphQLClient';
+	import { getEnTechsQuery } from '$lib/graphql/tech.query';
 	import { getEnTextsQuery } from '$lib/graphql/text.query';
+	import type Tech from '$lib/types/Tech.type';
 	import type Text from '$lib/types/Text.type';
 	import type { Load } from '@sveltejs/kit';
 
 	export const load: Load = async () => {
-		const [{ texts }] = await Promise.all([graphQLClient.request(getEnTextsQuery)]);
+		const [{ texts }, { techs }] = await Promise.all([
+			graphQLClient.request(getEnTextsQuery),
+			graphQLClient.request(getEnTechsQuery)
+		]);
 
 		return {
 			props: {
-				t: texts[0]
+				t: texts[0],
+				techs
 			}
 		};
 	};
@@ -20,8 +26,9 @@
 
 <script lang="ts">
 	export let t: Text;
+	export let techs: Tech[];
 </script>
 
 <HeaderComponent {t} />
 <AboutComponent {t} />
-<TechsComponent {t} />
+<TechsComponent {t} {techs} />
